f1fc2879b248d1135a6a224fa67f50ea1c6c751a
psr <psr@exilesoft.com>
2016-05-26 15:50:28 +0530

    .

---
+++ app.js
@@ -0,0 +1,29 @@
+ (function () {
+    'use strict';
+
+    angular.module('MyApp', [
+        'ui.router',
+        'MyApp.services',
+        'MyApp.controllers'
+   
+    ])
+        .config(function ($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise('/default');
+
+            $stateProvider
+                .state('default', {
+                    url: '/default',
+                    views: {
+                        '': {
+                            templateUrl: SitePrefix + 'views/firstView.html',
+                            controller: 'firstController'
+                        }
+                    }
+                })
+        })
+
+      
+    angular.module('MyApp.services', []);
+    angular.module('MyApp.controllers', []);
+}());
